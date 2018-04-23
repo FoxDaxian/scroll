228b06d5c3762f866629a3edf68834b630709c45
a13821190779 <945039036@qq.com>
2018-04-23 12:25:39 +0800

    feat: add autoprod change-log

--- commitlint.config.js
+++ commitlint.config.js
@@ -17,18 +17,18 @@ module.exports = {
             2,
             'always',
             [
-                'build',
-                'chore',
-                'ci',
-                'docs',
-                'feat',
-                'fix',
-                'perf',
-                'refactor',
-                'revert',
-                'style',
-                'test',
-				'update'
+                'build', // 构建工具或外部依赖的更改（npm，webpack，gulp等）
+                'chore', // 脏活累活
+                'ci', // 更改项目级的配置文件或脚本
+                'docs', // 文档更新
+                'feat', // 添加了一个新功能
+                'fix', // 修复了一个 bug
+                'perf', // 代码优化
+                'refactor', // 重构代码且不引进新的功能或修复 bug
+                'revert', // 撤销改动先前的提交
+                'style', //  不影响代码运行的更改（空格，格式，缺少分号等）
+                'test', // 添加或修改测试用例
+				'update' // 更新了某些东西
             ]
         ]
     }
